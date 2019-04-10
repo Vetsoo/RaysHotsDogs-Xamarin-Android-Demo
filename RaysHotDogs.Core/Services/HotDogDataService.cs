18f1ee488a5ad1a3f0a2b32ed37cc005cd6e70b1
Jente Vets <jentevets@gmail.com>
2019-04-10 21:15:54 +0200

    hotdog detail view and activity

---
+++ RaysHotDogs.Core/Services/HotDogDataService.cs
@@ -0,0 +1,37 @@
+﻿using RaysHotDogs.Core.Models;
+using RaysHotDogs.Core.Repositories;
+using System.Collections.Generic;
+
+namespace RaysHotDogs.Core.Services
+{
+    public class HotDogDataService
+    {
+        private static HotDogRepository hotDogRepository = new HotDogRepository();
+
+        public List<HotDog> GetAllHotDogs()
+        {
+            return hotDogRepository.GetAllHotDogs();
+        }
+
+        public List<HotDogGroup> GetGroupedHotDogs()
+        {
+            return hotDogRepository.GetGroupedHotDogs();
+        }
+
+        public List<HotDog> GetHotDogsForGroup(int hotDogGroupId)
+        {
+            return hotDogRepository.GetHotDogsForGroup(hotDogGroupId);
+        }
+
+        public List<HotDog> GetFavoriteHotDogs()
+        {
+            return hotDogRepository.GetFavoriteHotDogs();
+        }
+
+        public HotDog GetHotDogById(int hotDogId)
+        {
+            return hotDogRepository.GetHotDogById(hotDogId);
+        }
+    }
+}
+
